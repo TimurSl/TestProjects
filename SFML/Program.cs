9172ab25f687f153f71fb66b7e7c245dec3b0fe0
Timur Slinko <timurslinko1@gmail.com>
2023-05-10 19:27:14 +0300

    Initial commit

---
+++ SFML/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
+
+namespace RotatingHelloWorldSfmlDotNetCoreCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyWindow window = new MyWindow();
+            window.Show();
+        }
+    }
+
+    class MyWindow
+    {
+        private bool enabled = true;
+
+        public void Show()
+        {
+            VideoMode mode = new VideoMode(250, 250);
+            RenderWindow window = new RenderWindow(mode, "Circle");
+
+            window.Closed += (obj, e) => { window.Close(); };
+            window.KeyPressed +=
+                (sender, e) =>
+                {
+                    Window window = (Window)sender;
+                    if (e.Code == Keyboard.Key.Escape)
+                    {
+                        enabled = false;
+                        window.Close();
+                    }
+                };
+            
+            CircleShape shape = new CircleShape(100);
+            shape.FillColor = Color.White;
+            shape.Origin = new Vector2f(shape.Radius, shape.Radius);
+            shape.Position = new Vector2f(window.Size.X / 2, window.Size.Y / 2);
+
+
+            while (enabled)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.Green);
+                window.Draw(shape);
+                window.Display();
+            }
+        }
+    }
+}
